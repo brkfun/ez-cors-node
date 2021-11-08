49a75977c9e40904f4fcf2bdbc3e583abbc6b24e
BRKFun <burek@burek.com>
2021-11-09 01:05:47 +0300

    ez cors done refactored for es6

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
-var express = require('express'),
+let express = require('express'),
     request = require('request'),
     bodyParser = require('body-parser'),
     app = express();
+const url = require('url');
 
-var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+let myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
 console.log('Using limit: ', myLimit);
 
 app.use(bodyParser.json({limit: myLimit}));
 
 app.all('*', function (req, res, next) {
 
+    const queryObject = url.parse(req.url,true).query;
+
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    res.header("Access-Control-Allow-Headers", "*");
 
     if (req.method === 'OPTIONS') {
-        // CORS Preflight
         res.send();
     } else {
-        var targetURL = req.header('Target-URL');
+        let targetURL = queryObject.url;
         if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            res.send(400, { error: 'YA şu urlyi gönder!!!' });
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        if(!targetURL.includes('https://') && !targetURL.includes('http://')){
+            targetURL = 'http://' + targetURL;
+        }
+
+        request({ url: targetURL + req.url, method: req.method, json: req.body },
             function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
+            console.log('response',response);
+            console.log('body',body);
+            console.log('error', error);
 //                console.log(body);
             }).pipe(res);
     }
@@ -39,3 +45,5 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
     console.log('Proxy server listening on port ' + app.get('port'));
 });
+
+// 3000 portu kavsakproxy.izum domainine ayarlanacak bu domain ayarlanınca uygulama cors "free" çalışır
